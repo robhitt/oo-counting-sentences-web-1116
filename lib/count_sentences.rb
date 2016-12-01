b7c4a4dcfb14d4a30acdbcc9d3083d1e2faa70b1
Rob Hitt <robhitt@gmail.com>
2016-12-01 00:18:46 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,17 @@
-require 'pry'
-
 class String
-
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    self.split(" ").select { |word| word.sentence? || word.question? || word.exclamation?}.length
   end
 end
